b65744712a6de0acea4c2a22118dd65242143171
akshay <akshay.maity76@gmail.com>
2020-11-20 21:24:03 +0530

    add models in backend

---
+++ Backend-Code/model/user.js
@@ -0,0 +1,31 @@
+const mongoose=require('mongoose')
+
+const userSchema=new mongoose.Schema({
+
+    Name:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    email:{
+        type:String,
+        required:true,
+        trim:true,
+        unique:true
+    },
+    passsword:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    isAdmin:{
+        type:Boolean,
+        required:true,
+        default:false
+    }
+})
+
+
+const User=mongoose.model('User',userSchema,User)
+
+module.exports={User}
--- Backend-Code/server.js
+++ Backend-Code/server.js
@@ -38,3 +38,4 @@ const PORT=process.env.PORT || 5000.
 app.listen(PORT,()=>{
     console.log(`app is running on port ${PORT}`.rainbow)
 })
+
