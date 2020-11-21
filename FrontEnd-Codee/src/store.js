97a5f4d8c2427ed7df1b28a7df01c47ee4e6271f
akshay <akshay.maity76@gmail.com>
2020-11-21 20:01:57 +0530

    redux add in project

---
+++ FrontEnd-Codee/src/store.js
@@ -0,0 +1,20 @@
+import {applyMiddleware,createStore,combineReducers} from 'redux';
+import thunk from 'redux-thunk';
+import {composeWithDevTools} from 'redux-devtools-extension';
+
+
+const reducers=combineReducers({})
+
+const initialState={}
+
+const middleware=[thunk]
+
+
+const store=createStore(
+    reducers,
+    initialState,
+    composeWithDevTools(applyMiddleware(...middleware))
+)
+
+
+export default store;
