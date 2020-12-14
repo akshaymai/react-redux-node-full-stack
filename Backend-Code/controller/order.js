d7fc5dfd583bdffac40fe05204629de200e1d27c
=Akshay <=akshay.m@aviabird.com>
2020-12-14 17:47:37 +0530

    add update order status in backend

--- Backend-Code/controller/order.js
+++ Backend-Code/controller/order.js
@@ -63,5 +63,34 @@ getOrderById:  asyncHandeler(async(req,res)=>{
         res.status(404)
         throw new Error('Order found this id ')
     }
+    }),
+
+
+ 
+updateOrdertoPadid:  asyncHandeler(async(req,res)=>{
+ 
+    const order=await Order.findById(req.params.id)
+ 
+    if(order){
+      
+      order.isPaid=true;
+      order.paidAt=Date.now().toLocaleString()
+      order.paymentResult={
+
+        id: req.body.id,
+        status: req.body.status,
+        update_time: req.body.update_time,
+        email_address: req.body.email_address
+      }
+
+      const updatedOrder=await Order.save()
+      res.send(updatedOrder)
+    }else{
+        res.status(404)
+        throw new Error('Order status not upated ')
+    }
     })
-}
+}   
+
+
+
--- Backend-Code/routes/order.js
+++ Backend-Code/routes/order.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const router = express.Router()
-const {creatOrder,getOrderById} =require('../controller/order')
+const {creatOrder,getOrderById,updateOrdertoPadid} =require('../controller/order')
 const { auth } =require ('../middleware/authMiddleware')
 
 router.route('/create/order').post(auth, creatOrder);
 router.route('/get/order/:id').get(auth, getOrderById);
+router.route('/update/order/:id/paid').get(auth, updateOrdertoPadid);
+
 
 
 module.exports=router
